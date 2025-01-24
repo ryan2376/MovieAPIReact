f0af9d94096e98a0847b4f86dfcdfbc35b7647e9
ryan <ryannesh2376@gmail.com>
2025-01-24 21:03:50 +0300

    searchFxn

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -26,10 +26,22 @@ function Home() {
         loadPopularMovies();
     }, []);
 
-    const handleSearch = (e) => {
+    const handleSearch = async (e) => {
         e.preventDefault();
-        alert(searchQuery);
-        setSearchQuery("");
+        if (!searchQuery.trim())return
+        if (loading)return;
+
+        setLoading(true);
+        try{
+            const searchResults = await searchMovies(searchQuery)
+            setMovies(searchResults)
+            setError(null)
+        } catch (err){
+            console.log(err)
+            setError("Failed to search movies...");
+        } finally {
+            setLoading(false);
+        }
     };
 
     return(
