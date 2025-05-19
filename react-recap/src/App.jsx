c836051883a2a5027471f623ba14336cace24fa8
ryan <ryannesh2376@gmail.com>
2025-05-19 12:56:13 +0300

    conditional render 1

--- react-recap/src/App.jsx
+++ react-recap/src/App.jsx
@@ -3,20 +3,20 @@ import MovieCard from './components/MovieCard';
 
 function App() {
 
+  const movieNumber = 2;
+
   return (
     <>
-      <MovieCard movie = {{
-        title: "HummingBird",
-        release_date: '2025'
-      }} />
-      <MovieCard movie = {{
-        title: "Chronicles of Narnia",
-        release_date: '1998'
-      }} />
-      <MovieCard movie = {{
-        title: "HarryPotter",
-        release_date: '2006'
-      }} />
+      {movieNumber === 1 ? (
+        <MovieCard movie={{
+          title: "HummingBird",
+          release_date: '2025'
+        }} />) : (<MovieCard movie={{
+          title: "Chronicles of Narnia",release_date:'1998'}} 
+          />)
+      }
+
+
     </>
   )
 }
