c90d2f1f2c405d0a59535e348d0aaf4b9b9bf938
ryan <ryannesh2376@gmail.com>
2025-05-19 12:45:28 +0300

    components

--- react-recap/src/App.jsx
+++ react-recap/src/App.jsx
@@ -1,24 +1,24 @@
-import './App.css'
+import './App.css';
+import MovieCard from './components/MovieCard';
 
 function App() {
 
   return (
-    <div>
-      <p>Hello World</p>
-      <Greeting />
-      <Greeting display="Whats upp" />
-    </div>
+    <>
+      <MovieCard movie = {{
+        title: "HummingBird",
+        release_date: '2025'
+      }} />
+      <MovieCard movie = {{
+        title: "Chronicles of Narnia",
+        release_date: '1998'
+      }} />
+      <MovieCard movie = {{
+        title: "HarryPotter",
+        release_date: '2006'
+      }} />
+    </>
   )
 }
 
 export default App
-
-
-function Greeting({display}) {
-  return(
-    <>
-      <div>Hello Ryan</div>
-      <p>{display}</p>
-    </>
-  )
-}
