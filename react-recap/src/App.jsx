7ebffb9506893c93947b0485ea9e7799c90a810a
ryan <ryannesh2376@gmail.com>
2025-05-20 21:30:59 +0300

    favorites page

--- react-recap/src/App.jsx
+++ react-recap/src/App.jsx
@@ -1,22 +1,12 @@
 import './App.css';
 import MovieCard from './components/MovieCard';
+import Home from './pages/Home';
 
 function App() {
 
-  const movieNumber = 2;
-
   return (
     <>
-      {/* {movieNumber === 1 ? (
-        <MovieCard movie={{
-          title: "HummingBird",
-          release_date: '2025'
-        }} />) : (<MovieCard movie={{
-          title: "Chronicles of Narnia",release_date:'1998'}} 
-          />)
-      } */}
-      {movieNumber === 1 && <MovieCard movie={{ title: "HummingBird", release_date: '2025' }} />}
-
+      <Home />
     </>
   )
 }
