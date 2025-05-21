73e9e4b3baae075242c9aecc339ab2528a79e6be
ryan <ryannesh2376@gmail.com>
2025-05-21 13:28:12 +0300

    upd

--- react-recap/src/App.jsx
+++ react-recap/src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css';
+import Favorites from './pages/Favorites';
 import Home from "./pages/Home";
+import NavBar from "./components/Navbar";
+
+import { Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-  <>
-    <Home />
-  </>
+    <div>
+      <NavBar />
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/favorites' element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
---
+++ react-recap/src/pages/Favorites.jsx
@@ -0,0 +1,9 @@
+
+function Favorites() {
+    
+    return <div className="favorites-page">
+        <h2>No favorites yet</h2>
+    </div>
+}
+
+export default Favorites
