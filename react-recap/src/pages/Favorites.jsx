cd48b8fd653ec33da40fb740bec9957fcfdadc6a
ryan <ryannesh2376@gmail.com>
2025-05-21 13:45:29 +0300

    styles imports

--- react-recap/src/pages/Favorites.jsx
+++ react-recap/src/pages/Favorites.jsx
@@ -1,7 +1,9 @@
+import '../css/Favorites.css'
+
 
 function Favorites() {
     
-    return <div className="favorites-page">
+    return <div className="favorites-empty">
         <h2>No favorites yet</h2>
     </div>
 }
