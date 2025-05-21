cb38403d10a957cc78ee86fbca7764aa1921a5e7
ryan <ryannesh2376@gmail.com>
2025-05-21 12:42:05 +0300

    state Intro

--- react-recap/src/pages/Home.jsx
+++ react-recap/src/pages/Home.jsx
@@ -1,19 +1,32 @@
-import MovieCard from "../components/MovieCard"
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home() {
 
+    const [searchQuery, setSearchQuery] = useState("");
     const movies = [
         { id: 1, title: 'MockingBird', release_date: '2010' },
         { id: 2, title: 'Star-Wars', release_date: '1989' },
         { id: 3, title: 'Iron-Man', release_date: '2009' },
     ]
 
+    const handleSearch = (e) => {
+        alert(searchQuery)
+        e.preventDefault()
+        setSearchQuery("")
+    };
+
     return <div className="home">
+        <form onSubmit={handleSearch} className="search-form">
+            <input type="text" placeholder="Search for movies..." className="search-input" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
+            <button type="submit" className="search-button">Search</button>
+            
+        </form>
         <div className="movies-grid">
-            {movies.map((movie) => 
-                (
-                    <MovieCard movie={movie} key={movie.id} />
-                )
+            {movies.map((movie) =>
+            (
+                <MovieCard movie={movie} key={movie.id} />
+            )
             )}
         </div>
     </div>
