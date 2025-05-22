ca5ae35922e94f9c2cf9eb4c2a26046d91c24fa1
ryan <ryannesh2376@gmail.com>
2025-05-22 12:45:51 +0300

    bug1

--- react-recap/src/services/api.js
+++ react-recap/src/services/api.js
@@ -2,13 +2,14 @@
 const API_KEY = "adc001872706321cb1d4b2c95bc1a9bf";
 const BASE_URL = 'https://api.themoviedb.org/3';
 
-const getPopularMovies = async () => {
+export const getPopularMovies = async () => {
         const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
         const data = await response.json()
         return data.results
 }
-const searchMovies = async (query) => {
-        const response = await fetch(`${BASE_URL}/search/movie/?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
-        const data = await response.json()
+export const searchMovies = async (query) => {
+        const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+        const data = await response.json();
         return data.results
 }
+
